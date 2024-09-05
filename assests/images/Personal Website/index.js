01beebc446744b21c13da5b1ea9cebe3e4f7f893
Acer@vincepagaran <kaitopagaran@gmail.com>
2024-09-05 17:19:58 +0800

    first commit

---
+++ assests/images/Personal Website/index.js
@@ -0,0 +1,42 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+})
+
+let intro = document.querySelector('.intro')
+    let logo = document.querySelector('.logo-header')
+    let logoSpan = document.querySelectorAll('.logo')
+    window.addEventListener('DOMContentLoaded',()=>{
+      setTimeout(()=>{
+        logoSpan.forEach((span,idx) => {
+          setTimeout(()=>{
+            span.classList.add('active')
+        }, (idx+1)*400);
+      });
+
+      setTimeout(()=>{
+        logoSpan.forEach((span,idx)=>{
+          setTimeout(()=>{
+            span.classList.remove('active');
+            span.classList.add('fade');
+          }, (idx+1)*50)
+        })
+      },2000);
+      setTimeout(() => {
+        intro.style.top= '-100vh'
+      },2300);
+    })
+  })
